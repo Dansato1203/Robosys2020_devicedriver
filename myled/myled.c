c84e729cd46f861783ecbe199f89569b1ffbc6a7
Dan Sato <53420676+Dansato1203@users.noreply.github.com>
2020-12-02 12:20:18 +0900

    Update myled.c

--- myled/myled.c
+++ myled/myled.c
@@ -147,11 +147,11 @@ static int __init init_mod(void)
         
 
         for(i = 0;i < num; i++){
-        const u32 led = list[i];
-        const u32 index = led/10;
-        const u32 shift = (led%10)*3;
-        const u32 mask = ~(0x7 << shift);
-        gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+          const u32 led = list[i];
+          const u32 index = led/10;
+          const u32 shift = (led%10)*3;
+          const u32 mask = ~(0x7 << shift);
+          gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
         }
 
         return 0;
